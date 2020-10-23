959de18f5d8d94650b37332fc720fbff30860302
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-23 16:33:25 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData =[
+    {
+        question:'What type of language is Javascript',
+        a:'single threaded',
+        b:'multi threaded',
+        c:'both',
+        d:'none of above',
+        correct:'a'
+
+    },{
+        question:"What does HTML stands for",
+        a:'Json object Notation',
+        b:'Cascading style sheet',
+        c:'HyperText Markup language',
+        d:'HyperText Notation',
+        correct:'c'
+    },{
+        question:"Which syntax is used for coloring  the html element text",
+        a:'background-color',
+        b:'color',
+        c:'text-color',
+        d:'none of the above',
+        correct:'b'
+    },
+    {
+        question:"What does JSON stands for ",
+        a:'Json object Notation',
+        b:'Cascading style sheet',
+        c:'HyperText Markup language',
+        d:'Javascript ObjectNotation',
+        correct:'d'
+    },
+    {
+        question:"What does REST stands for",
+        a:'Json object Notation',
+        b:'Representational state transfer,
+        c:'HyperText Markup language',
+        d:'Rest state Transfer',
+        correct:'b'
+    }
+
+]
